b08f5c72e3ca966c98d65bf17628faebd111f669
Manish Sharma <manish.sharma0398@gmail.com>
2021-03-17 21:55:26 +0530

    error message for requests added

--- controllers/productController.js
+++ controllers/productController.js
@@ -10,10 +10,12 @@ exports.addNewProductController = async (req, res) => {
   });
 
   try {
-    product = await newProduct.save();
+    const product = await newProduct.save();
     res.status(201).json(product);
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    res
+      .status(503)
+      .json({ error: "Something went wrong. Please try again later" });
   }
 };
 
@@ -21,8 +23,10 @@ exports.getAllProducts = async (req, res) => {
   try {
     const products = await ProductSchema.find();
     res.status(200).json(products);
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    res
+      .status(503)
+      .json({ error: "Something went wrong. Please try again later" });
   }
 };
 
@@ -30,7 +34,7 @@ exports.getAProduct = async (req, res) => {
   try {
     const product = await ProductSchema.findById({ _id: req.params.id });
     res.status(200).json(product);
-  } catch (error) {
+  } catch (err) {
     res.status(404).json({ error: "Product not found" });
   }
 };
